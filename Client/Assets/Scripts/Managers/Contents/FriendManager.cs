311dece442d6e228d767c30a51a6c88e6b672a38
midnight774 <midnight774@dongguk.edu>
2022-11-29 05:42:51 +0900

    ADD Direct Message Through FriendManager setting

--- Client/Assets/Scripts/Managers/Contents/FriendManager.cs
+++ Client/Assets/Scripts/Managers/Contents/FriendManager.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static Struct;
 
 public class FriendManager
 {
     private List<string> m_FriendList;
     private List<string> m_FriendRequestList;
+    private Dictionary<string, FriendData> m_FriendDataDict;
 
     public void Init()
     {
@@ -16,11 +18,15 @@ public class FriendManager
         //친구 요청 받아와서 넣어줘야함
         m_FriendRequestList = new List<string>();
         InitFriendRequestList();
+
+        m_FriendDataDict = new Dictionary<string, FriendData>();
+        InitFriendData();
+
     }
 
     public void Update()
     {
-        
+
     }
 
     private void InitFriendList()
@@ -33,6 +39,74 @@ public class FriendManager
         m_FriendList.Add("Test3");
     }
 
+    private void InitFriendData()
+    {
+        for (int i = 0; i < m_FriendList.Count; ++i)
+        {
+            FriendData Data = new FriendData();
+            Data.FriendName = m_FriendList[i];
+            Data.DirectMessageList = new List<DirectMessageStruct>();
+            m_FriendDataDict.Add(Data.FriendName, Data);
+        }
+    }
+
+    public void SendDirectMessage(DirectMessageStruct SendMessageData)
+    {
+        string Sender = SendMessageData.SenderUser;
+        string Receiver = SendMessageData.ReceiverUser;
+
+        if (m_FriendDataDict.ContainsKey(Receiver))
+        {
+            FriendData Data = m_FriendDataDict[Receiver];
+
+            DirectMessageStruct MessageData = new DirectMessageStruct();
+            MessageData.ChattingText = SendMessageData.ChattingText;
+            MessageData.SenderUser = SendMessageData.SenderUser;
+            MessageData.ReceiverUser = SendMessageData.ReceiverUser;
+
+            Data.DirectMessageList.Add(MessageData);
+
+            //여기서 DirectChatting 패킷 보내야함
+
+        }
+
+    }
+
+    public void ReceiveDirectMessage(DirectMessageStruct ReceiveMessageData)
+    {
+        string Sender = ReceiveMessageData.SenderUser;
+        string Receiver = ReceiveMessageData.ReceiverUser;
+
+        //만약 다른 유저에게 향하는 메세지일경우 무시
+        if (Managers.Data.GetCurrentUser() != Receiver)
+            return;
+
+        //친구리스트에 등록된 유저로부터의 메세지일 경우 데이터를 복사해 추가
+        if(m_FriendDataDict.ContainsKey(Sender))
+        {
+            FriendData SenderData = m_FriendDataDict[Sender];
+
+            DirectMessageStruct MessageData = new DirectMessageStruct();
+            MessageData.ChattingText = ReceiveMessageData.ChattingText;
+            MessageData.SenderUser = ReceiveMessageData.SenderUser;
+            MessageData.ReceiverUser = ReceiveMessageData.ReceiverUser;
+
+            SenderData.DirectMessageList.Add(MessageData);
+        }
+    }
+
+    public FriendData GetFriendData(string FriendName)
+    {
+        if (!m_FriendDataDict.ContainsKey(FriendName))
+        {
+            //친구리스트에 없는 유저일경우 더미 데이터 리턴 (받는 입장에서 내용물 확인해야함)
+            FriendData DummyData = new FriendData();
+            return DummyData;
+        }
+        
+        return m_FriendDataDict[FriendName];
+    }
+
     private void InitFriendRequestList()
     {
         //db에서 친구요청 리스트 얻어와서 추가
@@ -40,7 +114,7 @@ public class FriendManager
 
     public void AddFreind(string FriendName)
     {
-        for(int i = 0;i<m_FriendList.Count;++i)
+        for (int i = 0; i < m_FriendList.Count; ++i)
         {
             if (m_FriendList[i] == FriendName)
                 return;
@@ -59,7 +133,7 @@ public class FriendManager
 
     public bool FindFriend(string FriendName)
     {
-        for(int i = 0;i<m_FriendList.Count;++i)
+        for (int i = 0; i < m_FriendList.Count; ++i)
         {
             if (m_FriendList[i] == FriendName)
                 return true;
@@ -80,7 +154,7 @@ public class FriendManager
     }
 
     public List<string> GetFriendRequestList()
-    { 
+    {
         return m_FriendRequestList;
     }
 }
